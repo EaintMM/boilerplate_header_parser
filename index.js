e76710396c17033cb0e6afd5f2aabfee09d45213
Eaint MM <eaintmyintmon@gmail.com>
2024-04-06 16:13:44 +0000

    complete

--- index.js
+++ index.js
@@ -20,8 +20,17 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  //const clientIP = req.headers['x-forwarded-for']
+  const clientIP = req.socket.remoteAddress;
+  const lang = req.headers['accept-language'];
+  //const userAgent = req.headers['user-agent'];
+  const browsedSoftware = req.headers['user-agent'];
+  res.json({ 
+    "ipaddress": clientIP,
+    "language": lang,
+    "software": browsedSoftware
+  });
 });
 
 // listen for requests :)
